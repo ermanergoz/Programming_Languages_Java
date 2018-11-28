a245053a1f50c5ecd2be13d2f5b036eabd8718b6
hacityler <erman.ergoz@hotmail.com>
2018-11-28 21:53:34 +0100

    28.11.2018

--- Week_4/assignment1/src/Circle.java
+++
@@ -1,8 +0,0 @@
-
-
-public class Circle extends Polygons{
-    public Circle(int radius, int xAxis, int yAxis) {
-        super(radius, xAxis, yAxis);
-    }
-
-}
--- Week_7/Bus.java
+++
@@ -1,9 +0,0 @@
-
-
-public class Bus{
-	public static void main (String args[]){
-		Passenger passenger = new Passenger("Josh", "Snow",123456);
-		System.out.println(passenger);
-	}
-
-	}
